216da809ffba0937a8b1654439a36ca945b51430
Kelvin Jones <pcmagic5000@gmail.com>
2015-11-22 22:40:43 -0700

    add barryvdh/laravel-ide-helper package

--- config/app.php
+++ config/app.php
@@ -145,6 +145,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Laravel Ide Helper
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
