9804a6b03fe538cbc7a99d2b50e054772f017a55
Kelvin Jones <pcmagic5000@gmail.com>
2015-12-13 22:32:03 -0700

    finish refactoring makes perfect

--- app/Http/Requests/AddPhotoRequest.php
+++ app/Http/Requests/AddPhotoRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use App\Flyer;
 
-class ChangeFlyerRequest extends Request
+class AddPhotoRequest extends Request
 {
     /**
      * Determine if the user is authorized to make this request.
--- resources/views/flyers/show.blade.php
+++ resources/views/flyers/show.blade.php
@@ -14,30 +14,31 @@
             <div class="description">{!! nl2br($flyer->description) !!}</div>
         </div>
 
-       <div class="col-md-8 gallery">
-           @foreach($flyer->photos->chunk(4) as $set)
-               <div class="row">
-                   @foreach($set as $photo)
-                       <div class="col-md-3 gallery_image">
-                           <img src="/{{$photo->thumbnail_path}}" alt="">
-                       </div>
-                   @endforeach
-               </div>
-           @endforeach
-       </div>
+        <div class="col-md-8 gallery">
+            @foreach($flyer->photos->chunk(4) as $set)
+                <div class="row">
+                    @foreach($set as $photo)
+                        <div class="col-md-3 gallery_image">
+                            <img src="/{{$photo->thumbnail_path}}" alt="">
+                        </div>
+                    @endforeach
+                </div>
+            @endforeach
+
+            @if ($user && $user->owns($flyer))
+                <hr>
+
+                <form id="addPhotosForm"
+                      action="{{ route('store_photo_path', [$flyer->zip, $flyer->street]) }}"
+                      method="post"
+                      class="dropzone"
+                >
+                    {{ csrf_field() }}
+                </form>
+            @endif
+        </div>
     </div>
 
-    <hr>
-
-    <h2>Add Your Photos</h2>
-
-    <form id="addPhotosForm"
-          action="{{ route('store_photo_path', [$flyer->zip, $flyer->street]) }}"
-          method="post"
-          class="dropzone"
-    >
-        {{ csrf_field() }}
-    </form>
 @stop
 
 
